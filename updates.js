acc6b27fb8a397578436f57daff61d86c5e7d20d
Neaves Voltio <neaves@voltio.us>
2023-12-10 19:10:13 -0600

    v1.01.02

---
+++ updates.js
@@ -0,0 +1,12 @@
+// Aqui se registran todos los cambios realizados y subidos a github
+
+// Registra la ultima versión hecha commit
+
+let version = "v1.01.02"
+
+// Describe cada una de las versiones hechas commit
+let versionObject = {
+  'v1.01.02' : 'Se crea y sube el archivo update.js ',
+
+}
+ 
